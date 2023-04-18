61d5c1719dce7f11a8661547c69fba1dc7d47161
Edgar Robitaille <erobita1@jh.edu>
2023-04-18 17:39:49 -0400

    Added constructors, destructors, and setters

--- bounds.cpp
+++ bounds.cpp
@@ -1,3 +1,21 @@
 #include "bounds.h"
 
 // TODO: implement member functions
+
+// Default constructor 
+Bounds::Bounds () { xmin = 0; xmax = 0; ymin = 0; ymax = 0; }; 
+
+Bounds::Bounds(double xmin, double xmax, double ymin, double ymax) {
+    this->xmin = xmin;
+    this->xmax = xmax;
+    this->ymin = ymin;
+    this->ymax = ymax;
+}
+
+// Destructor 
+Bounds::~Bounds () {};
+
+void Bounds::set_xmin (double xmin) {this->xmin = xmin;};
+void Bounds::set_xmax (double xmax) {this->xmax = xmax;};
+void Bounds::set_ymin (double ymin) {this->ymin = ymin;};
+void Bounds::set_ymax (double ymax) {this->ymax = ymax;};
