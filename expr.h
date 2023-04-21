218299e73b4ae59f03b89897205d3938f1a40e4c
Sun Moon <gmoon3@jhu.edu>
2023-04-21 13:16:22 -0400

    Added expr header file

--- expr.h
+++ expr.h
@@ -8,6 +8,7 @@ class Expr {
 private:
   // TODO: field(s) to store links to child Expr nodes
 
+
   // value semantics are prohibited
   Expr(const Expr &);
   Expr &operator=(const Expr &);
@@ -19,8 +20,12 @@ public:
   virtual double eval(double x) const = 0;
 
   // TODO: add member functions
+  void addChild(Expr* child) { children.push_back(child); }
+  Expr* getChild(size_t i) const { return children[i]; }
+  size_t numChildren() const { return children.size(); }
 };
 
 // TODO: add derived expression classes
+class Expr : public 
 
 #endif // EXPR_H
