b2ea44aed4fd51455dd5b211fd620fdbfbb238ee
Edgar Robitaille <erobita1@jh.edu>
2023-04-21 18:26:21 -0400

    Fixing plot.h, adding functions and fills

--- plot.h
+++ plot.h
@@ -12,13 +12,14 @@
 class Plot {
 private:
   // TODO: add fields to represent plot bounds, functions, fills, etc.
-
   // value semantics are prohibited
   Plot(const Plot &);
   Plot &operator=(const Plot &);
   Bounds bound;
   int width;
   int height;
+  std::vector<Function*> functions;
+  std::vector<Fill*> fills;
 
 public:
   Plot();
@@ -33,6 +34,12 @@ public:
 
   void set_height(int height){this->height = height;}
   int get_height() const {return height;}
+
+  void add_function(Function* function){functions.push_back(function);}
+  const std::vector<Function*>& get_functions() const {return functions;}
+
+  void add_fill(Fill* fill){fills.push_back(fill);}
+  const std::vector<Fill*>& get_fills() const {return fills;}
   
 };
 
