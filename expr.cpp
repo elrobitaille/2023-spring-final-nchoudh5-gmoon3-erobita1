58cb130d7ae958d89adaecb5ab066e9611fb4d82
Sun Moon <gmoon3@jhu.edu>
2023-04-21 14:26:40 -0400

    Finished implementation of expr.h and expr.cpp

--- expr.cpp
+++ expr.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <stdexcept>
 #include "exception.h"
 #include "expr.h"
 
@@ -9,6 +10,46 @@ Expr::Expr() {
 
 Expr::~Expr() {
   // TODO: deallocate child nodes (if any)
+  for (size_t i = 0; i < children.size(); i++) {
+    delete children[i];
+  }
 }
 
 // TODO: implement member functions for derived classes
+double AddExpr::eval(double x) const {
+  return left_->eval(x) + right_->eval(x);
+}
+
+double SubExpr::eval(double x) const {
+  return left_->eval(x) - right_->eval(x);
+}
+
+double MultExpr::eval(double x) const {
+  return m_left->eval(x) * m_right->eval(x);
+}
+
+double DivExpr::eval(double x) const {
+  double denominator = m_right->eval(x);
+
+  if (denominator == 0) {
+    throw std::runtime_error("Divide by zero error");
+  }
+
+  return m_left->eval(x) / denominator;
+}
+
+double Pi::eval(double x) const {
+  return M_PI;
+}
+
+double LiteralNumber::eval(double x) const {
+    return m_value;
+}
+
+double Sin::eval(double x) const {
+    return sin(m_arg->eval(x));
+}
+
+double Cos::eval(double x) const {
+    return cos(m_arg->eval(x));
+}
--- expr.h
+++ expr.h
@@ -1,15 +1,15 @@
 #ifndef EXPR_H
 #define EXPR_H
 
+#include <cmath>
 #include <vector>
 
 // Base expression class
 class Expr {
 private:
   // TODO: field(s) to store links to child Expr nodes
-
-
   // value semantics are prohibited
+  std::vector<Expr*> children; // links to child Expr nodes
   Expr(const Expr &);
   Expr &operator=(const Expr &);
 
@@ -26,6 +26,138 @@ public:
 };
 
 // TODO: add derived expression classes
-class Expr : public 
+class AddExpr : public Expr {
+public:
+  // constructor
+  AddExpr(Expr *left, Expr *right) : left_(left), right_(right) {}
+
+  // destructor
+  ~AddExpr() {
+    delete left_;
+    delete right_;
+  }
+
+  // evaluation function
+  virtual double eval(double x) const;
+private:
+  // links to child nodes
+  Expr *left_;
+  Expr *right_;
+};
+
+class SubExpr : public Expr {
+public:
+  // constructor
+  SubExpr(Expr *left, Expr *right) : left_(left), right_(right) {}
+
+  // destructor
+  ~SubExpr() {
+    delete left_;
+    delete right_;
+  }
+
+  // evaluation function
+  virtual double eval(double x) const;
+private:
+  // links to child nodes
+  Expr *left_;
+  Expr *right_;
+};
+
+// MultExpr
+
+class MultExpr : public Expr {
+
+public:
+  MultExpr(Expr* left, Expr* right) : m_left(left), m_right(right) {}
+
+  ~MultExpr() {
+    delete m_left;
+    delete m_right;
+  }
+
+  virtual double eval(double x) const;
+
+private:
+  Expr* m_left;
+  Expr* m_right;
+};
+
+
+// DivExpr
+
+class DivExpr : public Expr {
+public:
+  DivExpr(Expr* left, Expr* right) : m_left(left), m_right(right) {}
+
+  ~DivExpr() {
+    delete m_left;
+    delete m_right;
+  }
+
+  virtual double eval(double x) const;
+private:
+  Expr* m_left;
+  Expr* m_right;
+};
+
+// X class
+class X : public Expr {
+public:
+  X() {}
+
+  double eval(double x) const {
+    return x;
+  }
+};
+
+// Pi class
+class Pi : public Expr {
+public:
+  Pi() {}
+
+  virtual double eval(double x) const;
+};
+
+// LiteralNumber class
+class LiteralNumber : public Expr {
+private:
+  double m_value;
+
+public:
+  LiteralNumber(double value) : m_value(value) {}
+
+  virtual double eval(double x) const;
+};
+
+// Sin class
+class Sin : public Expr {
+private:
+  Expr *m_arg;
+
+public:
+  Sin(Expr *arg) : m_arg(arg) {}
+
+  ~Sin() {
+    delete m_arg;
+  }
+
+  virtual double eval(double x) const;
+};
+
+// Cos class
+class Cos : public Expr {
+private:
+  Expr *m_arg;
+
+public:
+  Cos(Expr *arg) : m_arg(arg) {}
+
+  ~Cos() {
+    delete m_arg;
+  }
+
+  virtual double eval(double x) const;
+};
 
 #endif // EXPR_H
