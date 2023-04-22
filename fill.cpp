ab4308ac8144fd570a48d90e98b9e9ddcbff0b53
Edgar Robitaille <erobita1@jh.edu>
2023-04-22 12:50:44 -0400

    Added non-default constructors to fill and destructor

--- fill.cpp
+++ fill.cpp
@@ -1,9 +1,16 @@
 #include <cassert>
 #include "fill.h"
 
-// TODO: implement constructors
+// Constructor for FillAbove and FillBelow
+Fill::Fill(FillType fill_type, const std::string& fn_name1, double opacity, const Color& color)
+    : fill_type_(fill_type), fn_name1_(fn_name1), opacity_(opacity), color_(color) {
+}
 
+// Separate Constructor for FillBetween due to a different formatting 
+Fill::Fill(FillType fill_type, const std::string& fn_name1, const std::string& fn_name2, double opacity, const Color& color)
+    : fill_type_(fill_type), fn_name1_(fn_name1), fn_name2_(fn_name2), opacity_(opacity), color_(color) {
+}
+// Destructor 
 Fill::~Fill() {
 }
 
-// TODO: implement member functions
