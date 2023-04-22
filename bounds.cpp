0365c23bab062d7903d459fc4f7af8af0087d93f
Edgar Robitaille <erobita1@jh.edu>
2023-04-22 00:08:52 -0400

    Minor comment adding

--- bounds.cpp
+++ bounds.cpp
@@ -1,10 +1,9 @@
 #include "bounds.h"
 
-// TODO: implement member functions
-
 // Default constructor 
 Bounds::Bounds () { xmin = 0; xmax = 0; ymin = 0; ymax = 0; }; 
 
+// Non-default constructor
 Bounds::Bounds(double xmin, double xmax, double ymin, double ymax) {
     this->xmin = xmin;
     this->xmax = xmax;
@@ -15,6 +14,8 @@ Bounds::Bounds(double xmin, double xmax, double ymin, double ymax) {
 // Destructor 
 Bounds::~Bounds () {};
 
+
+// Getter and setter for the private variables positions
 void Bounds::set_xmin (double xmin) {this->xmin = xmin;};
 void Bounds::set_xmax (double xmax) {this->xmax = xmax;};
 void Bounds::set_ymin (double ymin) {this->ymin = ymin;};
