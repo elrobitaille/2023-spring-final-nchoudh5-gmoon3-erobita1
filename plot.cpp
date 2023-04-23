2962836756a1a15fa7daaab22d3a38e564f93646
Edgar Robitaille <erobita1@jh.edu>
2023-04-22 22:29:15 -0400

    Fixed type error and added beter comments to plot

--- plot.cpp
+++ plot.cpp
@@ -1,23 +1,21 @@
 #include <cassert>
 #include "plot.h"
 
-// TODO: initialize fields
 Plot::Plot() {
+  // Initialize the bounds to the default values of 0
   bound_ = Bounds();
   width_ = 0;
   height_ = 0; 
 }
 
 Plot::~Plot() {
-  // TODO: delete dynamically-allocated objects
-  for (int i = 0; i < functions_.size(); i++) {
+  // Dynamically deallocate all the functions and fills
+  for (std::size_t i = 0; i < functions_.size(); i++) {
     delete functions_[i];
   }
 
-  for (int i = 0; i < fills_.size(); i++) {
+  for (std::size_t i = 0; i < fills_.size(); i++) {
     delete fills_[i];
   }
 }
-
-// TODO: implementations of Plot member functions
 
