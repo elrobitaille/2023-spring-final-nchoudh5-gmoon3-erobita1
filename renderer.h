fdf10a7e9d496911881ae7b9fab5680fc4451428
Edgar Robitaille <erobita1@jh.edu>
2023-04-23 17:51:15 -0400

    Added helper function declarations

--- renderer.h
+++ renderer.h
@@ -16,7 +16,12 @@ private:
   Renderer &operator=(const Renderer &);
 
   void renderFills();
-  void renderFunctions();
+  //void renderFunctions();
+  Color color_blend(const Color& orig, const Color& fill, double alpha);
+  std::pair<double, double> pixel_to_XY(int i, int j, double x_min, double x_max, double y_min, double y_max, int width, int height);
+  int find_pixel_row(int j, const Expr *f, double x_min, double x_max, double y_min, double y_max, int width, int height);
+  const Function* get_func_name(const std::string& name, const std::vector<Function*>& functions);
+  bool is_valid_fill(const Fill* fill, double x, double y, const std::vector<Function*>& functions);
 
 public:
   Renderer(const Plot &plot);
