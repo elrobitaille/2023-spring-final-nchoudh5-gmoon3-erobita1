939e327792cc4dd700037a7e515d4eab916d35cc
Edgar Robitaille <erobita1@jh.edu>
2023-04-24 17:15:41 -0400

    Minor changes and comments added to renderer

--- renderer.h
+++ renderer.h
@@ -21,8 +21,8 @@ private:
   Color color_blend(const Color& orig, const Color& fill, double alpha);
   std::pair<double, double> pixel_to_XY(int i, int j, double x_min, double x_max, double y_min, double y_max, int width, int height);
   int find_pixel_row(int j, const Expr *f, double x_min, double x_max, double y_min, double y_max, int width, int height);
-  const Function* get_func_name(const std::string& name, const std::vector<Function*>& functions);
-  bool is_valid_fill(const Fill* fill, double x, double y, const std::vector<Function*>& functions);
+  const Function* get_func_name(const std::string& name);
+  bool is_valid_fill(const Fill* fill, double x, double y);
 
 public:
   Renderer(const Plot &plot);
