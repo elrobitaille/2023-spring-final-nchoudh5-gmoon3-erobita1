b4770eb0c442211bd6893d9a5b380ae79ab65720
Edgar Robitaille <erobita1@jh.edu>
2023-04-23 20:56:51 -0400

    Rewrote with helper functions for renderer implementations

--- renderer.cpp
+++ renderer.cpp
@@ -54,11 +54,43 @@ std::pair<double, double> Renderer::pixel_to_XY(int i, int j, double x_min, doub
   return std::make_pair(x, y);
 }
 
-int find_pixel_row(int j, const Expr *f, double x_min, double x_max, double y_min, double y_max, int width, int height) {
+int Renderer::find_pixel_row(int j, const Expr *f, double x_min, double x_max, double y_min, double y_max, int width, int height) {
   double x = x_min + (j /static_cast<double>(width)) * (x_max - x_min);
   double y = f->eval(x);
   int i = height - 1 - std::floor(((y - y_min) / (y_max - y_min)) * height);
   return i;
+}
+
+const Function* Renderer::get_func_name(const std::string& name, const std::vector<Function*>& functions) {
+    for (const Function* func : m_plot.get_functions()) {
+        if (func->get_name() == name) {
+            return func;
+        }
+    }
+    return nullptr;
+}
+
+bool Renderer::is_valid_fill(const Fill* fill, double x, double y, const std::vector<Function*>& functions) {
+  const Function* func1 = get_func_name(fill->get_fn_name1(), functions);
+  if (func1 == nullptr) {
+      return false;
+  }
+  double func1_value = func1->get_expr()->eval(x);
+
+  if (fill->get_fill_type() == FillType::ABOVE) {
+    return y >= func1_value;
+  } else if (fill->get_fill_type() == FillType::BELOW) {
+    return y <= func1_value;
+  } else if (fill->get_fill_type() == FillType::BETWEEN) {
+    const Function* func2 = get_func_name(fill->get_fn_name2(), functions);
+    if (func2 == nullptr) {
+      return false;
+    }
+    double func2_value = func2->get_expr()->eval(x);
+    return y >= func1_value && y <= func2_value;
+    }
+
+  return false;
 }
 
 
@@ -70,6 +102,7 @@ void Renderer::renderFills() {
   double x_max = plot_bounds.get_xmax();
   double y_min = plot_bounds.get_ymin();
   double y_max = plot_bounds.get_ymax();
+
   for (const Fill* fill : m_plot.get_fills()) {
     for (int i = 0; i < height; ++i) {
       for (int j = 0; j < width; ++j) {
@@ -77,37 +110,7 @@ void Renderer::renderFills() {
         double x = xy.first;
         double y = xy.second;
 
-        const Function* function1 = nullptr;
-          for (const Function* func : m_plot.get_functions()) {
-            if (func->get_name() == fill->get_fn_name1()) {
-              function1 = func;
-              break;
-          }
-        }
-
-        bool in_fill_area = false;
-
-        if (fill->get_fill_type() == FillType::ABOVE) {
-          if (y >= function1->get_expr()->eval(x)) {
-            in_fill_area = true;
-          }
-        } else if (fill->get_fill_type() == FillType::BELOW) {
-          if (y <= function1->get_expr()->eval(x)) {
-            in_fill_area = true;
-          }
-        } else if (fill->get_fill_type() == FillType::BETWEEN) {
-          const Function* function2 = nullptr;
-          for (const Function* func : m_plot.get_functions()) {
-            if (func->get_name() == fill->get_fn_name2()) {
-              function2 = func;
-              break;
-            }
-          }
-          
-          if (y >= function1->get_expr()->eval(x) && y <= function2->get_expr()->eval(x)) {
-            in_fill_area = true;
-          }
-        }
+        bool in_fill_area = is_valid_fill(fill, x, y, m_plot.get_functions());
 
         if (in_fill_area) {
           Color original_color = m_img->get_pixel(j, i);
