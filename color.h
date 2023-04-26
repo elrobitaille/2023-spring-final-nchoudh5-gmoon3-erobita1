2da02825fccecaf860030594f3b8070d9ef3fae3
Edgar Robitaille <erobita1@jh.edu>
2023-04-26 17:40:59 -0400

    Adding comments to color.h

--- color.h
+++ color.h
@@ -5,11 +5,11 @@
 #include <cassert>
 
 struct Color {
+  // RGB values: red, green, and blue parts of color, each is an 8-bit unsigned integer [0, 255]
   uint8_t r, g, b;
 
+  // Constructor for Color struct, initializes color components with provided values or zero if not provided 
   Color(uint8_t r = 0, uint8_t g = 0, uint8_t b = 0) : r(r), g(g), b(b) { }
-  // member functions for doing operations on Color values
-  // could be helpful
 };
 
 
