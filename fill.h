56ade55a8d929a0cb1998d0e152d1bba1cb1d298
Nikhil Choudhary <nikhilch@outlook.com>
2023-04-28 17:06:24 -0400

    Finsihed commenting for fill

--- fill.h
+++ fill.h
@@ -10,12 +10,19 @@
 #include "plot.h"
 #include "func.h"
 
-// Added enum class FillType to represent the type of fill: Above, Below, or Between
+/**
+ * @enum FillType
+ * @brief Enum class representing the type of fill: Above, Below, or Between.
+ */
 enum class FillType {ABOVE, BELOW, BETWEEN};
 
+/**
+ * @class Fill
+ * @brief Class representing a fill area in a plot, defined by fill type, function names, opacity, and color.
+ */
 class Fill {
 private:
-  // Added private member variables to represent the fill type, the function names, the opacity, and color
+  // Private member variables for fill type, function names, opacity, and color
   FillType fill_type_;
   std::string fn_name1_;
   std::string fn_name2_;
@@ -23,14 +30,14 @@ private:
   Color color_;
   
 public:
-  // Added constructors for each fill type (above, below, between), account for FillAbove and FillBelow, then FillBetween
+  // Constructors for each fill type (above, below, between)
   Fill(FillType fill_type, const std::string& fn_name1, double opacity, const Color& color);
   Fill(FillType fill_type, const std::string& fn_name1, const std::string& fn_name2, double opacity, const Color& color);
   
   // Destructor 
   ~Fill();
 
-  // Added appropriate member functions as getters
+  // Getter member functions
   FillType get_fill_type() const {return fill_type_;}
   const std::string& get_fn_name1() const {return fn_name1_;}
   const std::string& get_fn_name2() const {return fn_name2_;}
